2c02f2c2d9869ff9c3979efe607b3897f7026fcb
Bienvenido David <bdavid@teamextension.com>
2011-07-01 00:36:14 -0400

    Initial import.

---
+++ chrome/content/memoryrestart.js
@@ -0,0 +1,163 @@
+if ("undefined" == typeof(TeamEXtension)) {
+	var TeamEXtension = {};
+};
+
+TeamEXtension.MemoryRestart = {
+	onLoad: function() {
+		if (this.firstRun()) {
+			this.addToolbarButton();
+		}
+
+		this.checkVersion();
+		
+		var interval = 1*60*1000; // 1 minute
+		// var interval = 5*1*1000; // 5 seconds
+
+		this.refreshMemory();
+		window.setInterval(this.refreshMemory, interval);
+	},
+
+	refreshMemory: function()
+	{
+		var prefService = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
+		var memoryReporterManager = Cc["@mozilla.org/memory-reporter-manager;1"].getService(Ci.nsIMemoryReporterManager);
+
+		var memoryUsed = 0;
+		var e = memoryReporterManager.enumerateReporters();
+		while (e.hasMoreElements()) {
+			var mr = e.getNext().QueryInterface(Ci.nsIMemoryReporter);
+			if (mr.path == "malloc/mapped") {
+				memoryUsed = mr.memoryUsed;
+				break;
+			}
+		}
+
+		var memoryrestartToolbar = document.getElementById('memoryrestart-button');
+		var memoryrestartPanel = document.getElementById('memoryrestart-panel');
+		
+		var memoryUsedInMb = memoryUsed / (1024 * 1024);
+		memoryrestartPanel.label = memoryUsedInMb + "Mb";
+
+		var strings = document.getElementById("memoryrestart-strings");
+		var memoryLimit = prefService.getIntPref("extensions.memoryrestart.memorylimit");
+		if (memoryLimit < memoryUsedInMb) {
+			var autoRestart = prefService.getBoolPref("extensions.memoryrestart.autorestart");
+			if (autoRestart) {
+				// this.quit is not a function
+				// this.quit();
+				var observerService = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
+				var canceled = Cc["@mozilla.org/supports-PRBool;1"].createInstance(Ci.nsISupportsPRBool);
+				observerService.notifyObservers(canceled, "quit-application-requested", "restart");
+				 
+				if (canceled.data) return; // somebody canceled our quit request
+
+				var appStartup = Cc['@mozilla.org/toolkit/app-startup;1'].getService(Ci.nsIAppStartup);
+				appStartup.quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit);
+			} else {
+				var colorAboveLimit = prefService.getCharPref("extensions.memoryrestart.colorabovelimit");
+				memoryrestartPanel.style.color = colorAboveLimit;
+				memoryrestartPanel.tooltipText = strings.getString("extensions.memoryrestart.tooltip.high");
+				if (memoryrestartToolbar != null) {
+					memoryrestartToolbar.style.listStyleImage = "url('chrome://memoryrestart/skin/above16.png')";
+					memoryrestartToolbar.tooltipText = memoryUsedInMb + "Mb";
+				}
+			}
+		} else {
+			var colorBelowLimit = prefService.getCharPref("extensions.memoryrestart.colorbelowlimit");
+			memoryrestartPanel.style.color = colorBelowLimit;
+			memoryrestartPanel.tooltipText = strings.getString("extensions.memoryrestart.tooltip.normal");
+			if (memoryrestartToolbar != null) {
+				memoryrestartToolbar.style.listStyleImage = "url('chrome://memoryrestart/skin/below16.png')";
+				memoryrestartToolbar.tooltipText = memoryUsedInMb + "Mb";
+			}
+		}
+	},
+
+	restartFirefox: function()
+	{
+		var prompts = Cc["@mozilla.org/embedcomp/prompt-service;1"].getService(Ci.nsIPromptService);
+		var strings = document.getElementById("memoryrestart-strings");
+		
+		if (prompts.confirm(window, strings.getString("extensions.memoryrestart.label"), strings.getString("extensions.memoryrestart.prompt"))) {
+			this.quit();
+		}
+	},
+
+	logToConsole: function(message)
+	{
+		var consoleService = Cc["@mozilla.org/consoleservice;1"].getService(Ci.nsIConsoleService);
+		consoleService.logStringMessage(message);
+	},
+
+	firstRun: function() {
+		var prefService = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
+		var version = prefService.getCharPref("extensions.memoryrestart.version");
+		return version == "0";
+	},
+	
+	quit: function() {
+		var observerService = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
+		var canceled = Cc["@mozilla.org/supports-PRBool;1"].createInstance(Ci.nsISupportsPRBool);
+		observerService.notifyObservers(canceled, "quit-application-requested", "restart");
+		 
+		if (canceled.data) return; // somebody canceled our quit request
+
+		var appStartup = Cc['@mozilla.org/toolkit/app-startup;1'].getService(Ci.nsIAppStartup);
+		appStartup.quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit);
+
+		// Firefox 4
+		// Components.utils.import("resource://gre/modules/Services.jsm");
+		// if (Services.wm.getMostRecentWindow("navigator:browser").canQuitApplication()) {
+		//     var appStartup = Cc['@mozilla.org/toolkit/app-startup;1'].getService(Ci.nsIAppStartup);
+		//     appStartup.quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit);
+		// }
+	},
+
+	addToolbarButton: function() {
+		var toolbarId = "memoryrestart-button";
+		var afterId = "urlbar-container";
+		var navBar = document.getElementById("nav-bar");
+		var curSet = navBar.currentSet.split(",");
+
+		if (curSet.indexOf(toolbarId) == -1) {
+		    var pos = curSet.indexOf(afterId) + 1 || curSet.length;
+		    var set = curSet.slice(0, pos).concat(toolbarId).concat(curSet.slice(pos));
+
+		    navBar.setAttribute("currentset", set.join(","));
+		    navBar.currentSet = set.join(",");
+		    document.persist(navBar.id, "currentset");
+		    try {
+		        BrowserToolboxCustomizeDone(true);
+		    }
+		    catch (e) {}
+		}
+	},
+
+	checkVersion: function() {
+		try {
+		    // Firefox 4+
+		    Components.utils.import("resource://gre/modules/AddonManager.jsm");
+		    AddonManager.getAddonByID("memoryrestart@teamextension.com", function(addon) {
+		    	TeamEXtension.MemoryRestart.checkVersionCallback(addon.version);
+		    });
+		} catch (e) {
+		    // Firefox 3.6
+		    var em = Cc["@mozilla.org/extensions/manager;1"].getService(Ci.nsIExtensionManager);
+		    var addon = em.getItemForID("memoryrestart@teamextension.com");
+		    this.checkVersionCallback(addon.version);
+		}
+	},
+	
+	checkVersionCallback: function(version) {
+		var prefService = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
+		var prevVersion = prefService.getCharPref("extensions.memoryrestart.version");
+		
+		if (version != prevVersion) {
+			prefService.setCharPref("extensions.memoryrestart.version", version);
+			var strings = document.getElementById("memoryrestart-strings");
+			gBrowser.selectedTab = gBrowser.addTab(strings.getString("extensions.memoryrestart.url"));
+		}
+	}
+};
+
+window.addEventListener("load", function(e) { TeamEXtension.MemoryRestart.onLoad(); }, false);
---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,8 @@
+pref("extensions.memoryrestart.version", "0");
+pref("extensions.memoryrestart.memorylimit", 500);
+pref("extensions.memoryrestart.colorabovelimit", "red");
+pref("extensions.memoryrestart.colorbelowlimit", "black");
+pref("extensions.memoryrestart.autorestart", false);
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.memoryrestart@teamextension.com.description", "chrome://memoryrestart/locale/memoryrestart.properties");
