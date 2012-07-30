a9ea7844dfe329db3367db5aa1870508db95893f
John Reyes <johnlreyes@yahoo.com>
2012-07-30 13:26:13 +0800

    1.10

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,5 +1,5 @@
 pref("extensions.memoryrestart.version", "0");
-pref("extensions.memoryrestart.memorylimit", 250);
+pref("extensions.memoryrestart.memorylimit", 500);
 pref("extensions.memoryrestart.minimummemorylimit", 250);
 pref("extensions.memoryrestart.colorabovelimit", "red");
 pref("extensions.memoryrestart.colorbelowlimit", "black");
